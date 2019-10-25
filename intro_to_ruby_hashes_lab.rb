0a7ca4484ad208cb247d5cebf4f37253a52d23a8
Dardan Gjonbalaj <xdundun@gmail.com>
2019-10-25 14:32:44 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ end
 
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
-  key[value]
+  my_hash_creator[:key]
 end
 
 def read_from_hash(hash, key)
